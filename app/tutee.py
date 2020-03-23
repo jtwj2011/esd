e09d44b510d06183a0bfa102082c448deec973b3
Yan Sheng <ystan.2018@sis.smu.edu.sg>
2020-03-23 18:16:17 +0800

    Updated Tutee.py

--- app/tutee.py
+++ app/tutee.py
@@ -46,8 +46,10 @@ def find_by_email(email):
         return jsonify(tutee.json())
     return jsonify({"message": "Tutee not found"}), 404
 
+
+
 @app.route("/tutee/<string:email>", methods=['POST'])
-def register(isbn13):
+def create_tutee_profile():
     if (Tutee.query.filter_by(email=email).first()):
         return jsonify({"message": "A tutee with username '{}' already exists.".format(username)}), 400
 
@@ -61,6 +63,56 @@ def register(isbn13):
         return jsonify({"message": "An error occurred registering the tutee."}), 500
 
     return jsonify(tutee.json()), 201
+
+#attritubutes that can be updated:
+# email, contact, name, address, subject_rate
+@app.route("/tutee/<string:email>/<string:contact_number>/<string:password>/<string:fullname>/<string:address>/", methods=['POST'])
+def update_tutee_profile(email, contact_number, name, address, password):
+    if (Tutee.query.filter_by(email=email).first()):
+        tutee = Tutee.query.filter_by(email=email).first()
+        # issue: how do we check if the email is unique inside the database
+        if tutee.email != email:
+            try:
+                tutee.email = email
+                if (Tutee.query.filter_by(email=tutee.email).first()==False):
+                    db.session.commit()
+                else:
+                    return jsonify({"message": "Email already exists."}), 500
+            except:
+                return jsonify({"message": "An error occurred when updating the email."}), 500
+
+        if tutee.contact_number != contact_number:
+            try:
+                tutee.contact_number = contact_number
+                db.session.commit()
+            except:
+                return jsonify({"message": "An error occurred when updating the contact number."}), 500
+        
+        if tutee.password != password:
+            try:
+                tutee.password = password
+                db.session.commit()
+            except:
+                return jsonify({"message": "An error occurred when updating the password."}), 500
+
+        if tutee.name != name:
+            try:
+                tutee.name = name
+                db.session.commit()
+            except:
+                return jsonify({"message": "An error occurred when updating the name."}), 500
+
+        if tutee.address != address:
+            try:
+                tutee.address = address
+                db.session.commit()
+            except:
+                return jsonify({"message": "An error occurred when updating the address."}), 500
+
+        return jsonify(tutee.json()), 201
+
+    else:
+        return jsonify({"message": "User does not exist in the system."}), 400
 
 # basically prevents another file from doing app.run even if they import tutee.py
 # because then __name__ is tutee.py
