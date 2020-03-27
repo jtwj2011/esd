3c49fa0a445dc757ea896dfce830a6cf474b8911
saranpals <saranpals.2018@sis.smu.edu.sg>
2020-03-27 17:40:28 +0800

    booking update

--- app/booking/booking.py
+++ app/booking/booking.py
@@ -18,6 +18,7 @@ class Booking(db.Model):
     tutee_id = db.Column(db.String(64), nullable=False)
     payment = db.Column(db.Float(precision=2), nullable=False)
     subjects = db.Column(db.String(64), nullable=False)
+    status = db.Column(db.String(64), nullable=False)
 
     def __init__(self, booking_id, tutor_id, tutee_id, payment, subjects):
         self.booking_id = booking_id
@@ -25,15 +26,31 @@ class Booking(db.Model):
         self.tutee_id = tutee_id
         self.payment = payment
         self.subjects = subjects
+        self.status = status
 
     def json(self):
-        return {"booking_id": self.booking_id, "tutor_id": self.tutor_id, "tutee_id": self.tutee_id, "payment": self.payment, "subjects": self.subjects}
+        return {"booking_id": self.booking_id, "tutor_id": self.tutor_id, "tutee_id": self.tutee_id, "payment": self.payment, "subjects": self.subjects, "status": self.status}
 
 
 @app.route("/booking")
 def get_all():
     return jsonify({"Bookings": [booking.json() for booking in Booking.query.all()]})
 
+@app.route("/booking/<string:tutee_id>")
+def get_all_bookings_for_tutee(tutee_id):
+    return jsonify({"Bookings": [booking.json() for booking in Booking.query.filter_by(tutee_id=tutee_id)]})
+
+@app.route("/booking/<string:tutor_id>")
+def get_all_bookings_for_tutor(tutor_id):
+    return jsonify({"Bookings": [booking.json() for booking in Booking.query.filter_by(tutor_id=tutor_id)]})
+
+@app.route("/booking/<string:status>")
+def get_all_bookings_based_status(status):
+    bookings = Booking.query.filter_by(status=status)
+    #if bookings:
+    return jsonify({"Bookings": [booking.json() for booking in Booking.query.all()]})
+    #return jsonify({"message": "No Bookings available." }), 400
+    
 
 @app.route("/booking/<string:booking_id>")
 def find_by_booking_id(booking_id):
