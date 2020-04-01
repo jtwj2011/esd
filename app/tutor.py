2abfa50e94f64eb2a0caf0b28681a60d6036c2b3
Yan Sheng <ystan.2018@sis.smu.edu.sg>
2020-04-01 15:49:10 +0800

    Updated tutee and tutor

--- app/tutor.py
+++ app/tutor.py
@@ -23,7 +23,7 @@ CORS(app)
 class Tutor(db.Model):
     __tablename__ = 'tutor'
     tutor_id = db.Column(db.String(24), primary_key=True)
-    contact_number = db.Column(db.String(8), nullable=False)
+    tutor_contact_number = db.Column(db.String(8), nullable=False)
     name = db.Column(db.String(12), nullable=False)
     level = db.Column(db.String(50), nullable=False)
     subject = db.Column(db.String(50), primary_key = True)
@@ -177,7 +177,7 @@ def filter_by_gender(gender):
 
 
 @app.route("/tutor/subject/<string:subject>")
-def filter_by_subject(subject):
+def filter_by_Tutor_subject(subject):
     tutor = Tutor.query.filter_by(subject=subject).first()
     if tutor:
         return jsonify({"Tutor": [tutor.json() for tutor in Tutor.query.filter_by(subject=subject).all()]})
@@ -186,13 +186,51 @@ def filter_by_subject(subject):
 
 
 @app.route("/tutor/level/<string:level>")
-def filter_by_levels(level):
+def filter_by_Tutor_levels(level):
     tutor = Tutor.query.filter_by(level=level).first()
     if tutor:
         return jsonify({"Tutor": [tutor.json() for tutor in Tutor.query.filter_by(level=level).all()]})
     return jsonify({"message": "Profile not found."}), 404
 
 
+def receiveRequest():
+    # prepare a queue for receiving messages
+    channelqueue = channel.queue_declare(queue="tutor", durable=True) # 'durable' makes the queue survive broker restarts so that the messages in it survive broker restarts too
+    queue_name = channelqueue.method.queue
+    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='*.request') # bind the queue to the exchange via the key
+        # any routing_key with two words and ending with '.order' will be matched
+
+    # set up a consumer and start to wait for coming messages
+    channel.basic_qos(prefetch_count=1) # The "Quality of Service" setting makes the broker distribute only one message to a consumer if the consumer is available (i.e., having finished processing and acknowledged all previous messages that it receives)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True) # 'auto_ack=True' acknowledges the reception of a message to the broker automatically, so that the broker can assume the message is received and processed and remove it from the queue
+    channel.start_consuming() # an implicit loop waiting to receive messages; it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
+
+def callback(channel, method, properties, body): # required signature for the callback; no return
+    print("Received an order by " + file)
+    result = processRequest(json.loads(body))
+    # print processing result; not really needed
+    json.dump(result, sys.stdout, default=str) # convert the JSON object to a string and print out on screen
+    print() # print a new line feed to the previous json dump
+    print() # print another new line as a separator
+
+def processRequest(request):
+    print("Processing a request:")
+    print(request)
+    # Can do anything here. E.g., publish a message to the error handler when processing fails.
+    resultstatus = bool(random.getrandbits(1)) # simulate success/failure with a random True or False
+    result = {'status': resultstatus, 'message': 'Simulated random s result.', 'order': order}
+    resultmessage = json.dumps(result, default=str) # convert the JSON object to a string
+    if not resultstatus: # inform the error handler when shipping fails
+        print("Failed shipping.")
+        # send the message to the eror handler
+        channel.queue_declare(queue='errorhandler', durable=True) # make sure the queue used by the error handler exist and durable
+        channel.queue_bind(exchange=exchangename, queue='errorhandler', routing_key='*.error') # make sure the queue is bound to the exchange
+        channel.basic_publish(exchange=exchangename, routing_key="shipping.error", body=resultmessage,
+            properties=pika.BasicProperties(delivery_mode = 2) # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange)
+        )
+    else:
+        print("OK shipping.")
+    return result
 
 
 #filter by level, subject, price range
